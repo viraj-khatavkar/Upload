ded9ab8ef0a86c2b3c2ae1a16a86817849c77767
Viraj Khatavkar <viraj.2438@gmail.com>
2015-07-03 13:23:09 +0530

    Tests and method for normalising $_FILES

--- spec/UploadSpec.php
+++ spec/UploadSpec.php
@@ -9,11 +9,46 @@ class UploadSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType('Viraj\Upload\Upload');
+        $this->shouldHaveType ('Viraj\Upload\Upload');
     }
 
     function it_should_return_an_exception_for_blank_file_name()
     {
-        $this->shouldThrow('Viraj\Upload\Exceptions\InvalidFileException')->during('file', ['']);
+        $this->shouldThrow ('Viraj\Upload\Exceptions\InvalidFileException')->during ('file', [ '' ]);
+    }
+
+    function it_should_return_normalized_multidimensional_array_for_native_multidimensional_array_of_files_()
+    {
+        $files = [
+            'files' => [
+                'name'  => [
+                    0 => 'demo 1',
+                    1 => 'demo 2'
+                ],
+                'type'  => [
+                    0 => 'image/jpeg',
+                    1 => 'image/jpeg'
+                ],
+                'error' => [
+                    0 => 0,
+                    1 => 1
+                ]
+            ]
+        ];
+
+        $expected_normalized_array = [
+            0 => [
+                'name'  => 'demo 1',
+                'type'  => 'image/jpeg',
+                'error' => 0
+            ],
+            1 => [
+                'name'  => 'demo 2',
+                'type'  => 'image/jpeg',
+                'error' => 1
+            ]
+        ];
+
+        $this->normalize ($files)->shouldReturn ($expected_normalized_array);
     }
 }
--- src/Upload.php
+++ src/Upload.php
@@ -8,9 +8,23 @@ class Upload
 {
     public function file($field_name)
     {
-        if (trim($field_name) == '')
-        {
+        if (trim ($field_name) == '') {
             throw new InvalidFileException('File seems to be corrupt or Invalid');
         }
+    }
+
+    public function normalize($argument)
+    {
+        $newfiles = [ ];
+
+        foreach ($argument as $fieldname => $fieldvalue):
+            foreach ($fieldvalue as $paramname => $paramvalue):
+                foreach ((array)$paramvalue as $index => $value):
+                    $newfiles[ $fieldname ][ $index ][ $paramname ] = $value;
+                endforeach;
+            endforeach;
+        endforeach;
+
+        return $newfiles[ 'files' ];
     }
 }
